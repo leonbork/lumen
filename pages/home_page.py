5ea69b6ab9d3309fabfe9f7e9e1d665c0a2e70ca
leonbork <47828424+leonbork@users.noreply.github.com>
2025-06-12 21:04:37 +0300

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,43 +1,34 @@
-from appium.webdriver.common.appiumby import AppiumBy
 from pages.base_page import BasePage
 import allure
 
 class HomePage(BasePage):
-    # Locators
-    HOME_TAB = (AppiumBy.ACCESSIBILITY_ID, "Home tab")
-    MESSAGES_TAB = (AppiumBy.ACCESSIBILITY_ID, "Messages tab")
-    PROFILE_TAB = (AppiumBy.ACCESSIBILITY_ID, "Profile tab")
-    COUNTER_VALUE = (AppiumBy.ACCESSIBILITY_ID, "Counter value")
-    RESET_COUNTER_BUTTON = (AppiumBy.ACCESSIBILITY_ID, "Reset Counter button")
-    LOGOUT_BUTTON = (AppiumBy.ACCESSIBILITY_ID, "Logout button")
-
     def __init__(self, driver):
         super().__init__(driver)
 
     @allure.step("Getting current counter value")
     def get_counter_value(self):
-        return int(self.get_element_text(self.COUNTER_VALUE))
+        return int(self.get_text_by_key("Counter value"))
 
     @allure.step("Clicking 'Reset Counter' button")
     def click_reset_counter(self):
-        self.click_element(self.RESET_COUNTER_BUTTON)
+        self.click_by_key("Reset Counter button")
 
     @allure.step("Clicking 'Logout' button")
     def click_logout(self):
-        self.click_element(self.LOGOUT_BUTTON)
+        self.click_by_key("Logout button")
 
     @allure.step("Navigating to Messages tab")
     def navigate_to_messages(self):
-        self.click_element(self.MESSAGES_TAB)
+        self.click_by_key("Messages tab")
 
     @allure.step("Navigating to Profile tab")
     def navigate_to_profile(self):
-        self.click_element(self.PROFILE_TAB)
+        self.click_by_key("Profile tab")
 
     @allure.step("Navigating to Home tab")
     def navigate_to_home(self):
-        self.click_element(self.HOME_TAB)
+        self.click_by_key("Home tab")
 
     @allure.step("Checking if Home tab is displayed")
     def is_home_tab_displayed(self):
-        return self.is_element_displayed(self.HOME_TAB)
+        return self.is_displayed_by_key("Home tab")
