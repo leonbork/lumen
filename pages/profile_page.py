a872fe12c814c25d82eba0b726a305a824a0196d
leonbork <47828424+leonbork@users.noreply.github.com>
2025-06-12 21:05:13 +0300

    Update profile_page.py

--- pages/profile_page.py
+++ pages/profile_page.py
@@ -1,13 +1,10 @@
-from appium.webdriver.common.appiumby import AppiumBy
 from pages.base_page import BasePage
 import allure
 
 class ProfilePage(BasePage):
-    PROFILE_SCREEN_TITLE = (AppiumBy.ACCESSIBILITY_ID, "Profile tab")
-
     def __init__(self, driver):
         super().__init__(driver)
 
     @allure.step("Checking if Profile screen is displayed")
     def is_profile_screen_displayed(self):
-        return self.is_element_displayed(self.PROFILE_SCREEN_TITLE)
+        return self.is_displayed_by_key("Profile tab")
