e037170e3a1bf7c7cd6b418cc47a83ec6ef57be6
leonbork <47828424+leonbork@users.noreply.github.com>
2025-06-12 21:05:34 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,28 +1,20 @@
-from appium.webdriver.common.appiumby import AppiumBy
 from pages.base_page import BasePage
 import allure
 
 class LoginPage(BasePage):
-    # Locators
-    EMAIL_FIELD = (AppiumBy.ACCESSIBILITY_ID, "Email field")
-    PASSWORD_FIELD = (AppiumBy.ACCESSIBILITY_ID, "Password field")
-    LOGIN_BUTTON = (AppiumBy.ACCESSIBILITY_ID, "Login button")
-    # Assuming this ID for the error message after validation
-    LOGIN_ERROR_MESSAGE = (AppiumBy.ACCESSIBILITY_ID, "Login error message")
-
     def __init__(self, driver):
         super().__init__(driver)
 
     @allure.step("Logging in with email '{email}' and password '{password}'")
     def login(self, email, password):
-        self.send_keys_to_element(self.EMAIL_FIELD, email)
-        self.send_keys_to_element(self.PASSWORD_FIELD, password)
-        self.click_element(self.LOGIN_BUTTON)
+        self.send_keys_by_key("Email field", email)
+        self.send_keys_by_key("Password field", password)
+        self.click_by_key("Login button")
 
     @allure.step("Getting login error message")
     def get_error_message(self):
-        return self.get_element_text(self.LOGIN_ERROR_MESSAGE)
+        return self.get_text_by_key("Login error message")
 
     @allure.step("Checking if Login button is displayed")
     def is_login_button_displayed(self):
-        return self.is_element_displayed(self.LOGIN_BUTTON)
+        return self.is_displayed_by_key("Login button")
